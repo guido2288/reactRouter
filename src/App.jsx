e4147c87daaf805e2b2506f3d339d5ad80cc2707
guido2288 <guido2288@gmail.com>
2023-12-12 19:17:37 -0300

    Se agrega componente Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import About from './Pages/About'
 import HomePage from './Pages/Home'
 import Page404 from './Pages/Page404'
+import { Route } from './Route'
 import { Router } from './Router'
+
+
 
 function App() {
 
   const appRoutes = [
-    {
-      path: '/',
-      Component: HomePage
-    },
-    {
-      path: '/about',
-      Component: About
-    },
     {
       path: '/search/:query',
       Component: ({ routeParams }) => <h1>Has buscado: {routeParams.query}</h1>
@@ -22,7 +17,11 @@ function App() {
 
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} />
+      <Router routes={appRoutes} defaultComponent={Page404} >
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={About} />
+      </Router >
+
     </main>
   )
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Children } from "react";
 import { EVENTS } from "./const";
 
 import { match } from "path-to-regexp";
 
-export function Router({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
+
+export function Router({ children, routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
   useEffect(() => {
@@ -23,7 +24,18 @@ export function Router({ routes = [], defaultComponent: DefaultComponent = () =>
 
   let routeParams = {};
 
-  const Page = routes.find(({ path }) => {
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const isRoute = name === 'Route'
+
+    if (!isRoute) return null;
+
+    return props
+  }).filter(Boolean)
+
+  const routeToUse = routes.concat(routesFromChildren);
+
+  const Page = routeToUse.find(({ path }) => {
     if (path == currentPath) return true;
 
     const matcherUrl = match(path, { decode: decodeURIComponent })
